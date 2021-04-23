88419089675935a126df0d74a5cf7ee00a7939a4
basafilm <54095122+basafilm@users.noreply.github.com>
2021-04-23 22:30:15 +0200

    Week_2 home work

---
+++ week2/week_2.py
@@ -0,0 +1,31 @@
+def fizz_buzz(number):
+    if number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    elif number % 3 == 0 & number % 5 == 0:
+        print('FizzBuzz')
+    else:
+        print(number)
+
+
+fizz_buzz(int(input()))
+
+lst = [5, 10, 20]
+try:
+    print(lst)
+except IndexError:
+    print("Something went wrong")
+
+
+class Jet:
+    def name(self):
+        return 'name', self
+
+    def country(self):
+        return 'country', self
+
+
+print(Jet.name('B-52'), Jet.country('USA'))
+
+print(Jet.name('MiG-21'), Jet.country('Russia'))
