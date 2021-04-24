8e69b886f8d9388265a8ec87a5d53ed9fcaa48f3
basafilm <54095122+basafilm@users.noreply.github.com>
2021-04-24 11:35:17 +0200

    Week_2 home work

--- week2/week_2.py
+++ week2/week_2.py
@@ -29,3 +29,15 @@ class Jet:
 print(Jet.name('B-52'), Jet.country('USA'))
 
 print(Jet.name('MiG-21'), Jet.country('Russia'))
+
+
+def show_numbers(limit):
+    for i in range(0, limit):
+        if i % 2 == 0:
+            print(i, 'EVEN')
+        else:
+            print(i, ' ODD')
+
+
+show_numbers(int(input()))
+
